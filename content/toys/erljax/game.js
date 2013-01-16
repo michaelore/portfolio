c2eb141d0f4b7d8e0f36bf6011eda30744a03b75
Michael Ore <selbram1993@gmail.com>
2013-01-15 21:01:25 -0600

    Added content for erljax

---
+++ content/toys/erljax/game.js
@@ -0,0 +1,45 @@
+var server = "/controller.yaws"
+var client_id
+function click(id) {
+    try {
+        var op;
+        if ($("#" + id).attr("src")=="images/x.png") {
+            op = "clickx";
+            var snd = new Audio('laser.wav');
+            snd.play();
+						update({"id": id, "src": "images/o.png"});
+        } else {
+            op = "clicko";
+            var snd = new Audio('ice.wav');
+            snd.play();
+						update({"id": id, "src": "images/x.png"});
+        }
+        //$.getJSON(server,
+        //        {'op': op, 'id': id},
+        //        update);
+    } catch(e) {
+        alert(e);
+    }
+}
+
+function update(data) {
+    $("#" + data.id).attr({'src': data.src});
+}
+
+function request_update() {
+    $.getJSON(server,
+            {'op': "update", 'id': client_id},
+            function(mat) {
+            $.each(mat, function(id, image) {update({'id': id, 'src': image});});
+            });
+}
+
+$(document).ready(function() {
+    $.getJSON(server,
+            {'op': "hello"},
+            function(obj) {
+            $.each(obj.mat, function(id, image) {update({'id': id, 'src': image});});
+            client_id = obj.id;
+            setInterval("request_update()", 100);
+            });
+});
